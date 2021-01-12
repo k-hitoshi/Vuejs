747c7919f31c9e2941c03e1e628c5e85a7211750
kusimura <kusimura@esm.co.jp>
2021-01-12 18:29:10 +0900

    応用課題対応

--- main.js
+++ main.js
@@ -1,104 +1,28 @@
-// step02_ローカルストレージ API の使用
-var STORAGE_KEY = 'todos-vuejs-demo'
-var todoStorage = {
-    fetch: function () {
-        var todos = JSON.parse(
-            localStorage.getItem(STORAGE_KEY) || '[]'
-        )
-        todos.forEach(function (todo, index) {
-            todo.id = index
-        })
-        todoStorage.uid = todos.length
-        return todos
-    },
-    save: function (todos) {
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+function doGet() {
+  return HtmlService.createTemplateFromFile('index').evaluate();
+}
+
+function inputSheet(todosT) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
+  let lastrow = sheet.getLastRow();
+  let lastColumn = sheet.getLastColumn();
+  sheet.getRange(lastrow + 1, 1, 1, lastColumn).setValues(todosT);
+}
+
+function deleteSheet(index) {
+ const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
+ sheet.deleteRows(index + 2, 1);
+}
+
+function changeState(indexC) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
+  let range   = sheet.getRange(indexC + 2, 3).getValue();
+  let lastColumn = sheet.getLastColumn();
+  if (range === 0) {
+    sheet.getRange(indexC + 2, lastColumn).setValue(1)
+    } else {
+      sheet.getRange(indexC + 2, lastColumn).setValue(0)
     }
 }
 
-// step01_インスタンスの作成
-const app = new Vue({
-    el: '#app',
-
-    data: {
-        // step05_リストレンダリング
-        todos: [],
-        current: -1,
-        options: [
-            { value: -1, label: 'すべて' },
-            { value: 0, label: '作業中' },
-            { value: 1, label: '完了' }
-        ]
-    },
-
-    // step12_リストの絞り込み機能
-    computed: {
-
-        computedTodos: function() {
-            return this.todos.filter(function(el) {
-                return this.current < 0 ? true : this.current === el.state
-            }, this)
-        },
-
-        labels() {
-            return this.options.reduce(function (a, b) {
-                return Object.assign(a, { [b.value]: b.label })
-            },{})
-            // キーから見つけやすいように、次のように加工したデータを作成
-            // {0: '作業中', 1: '完了', -1: 'すべて'}
-        }
-    },
-
-    // step08_ストレージへの保存の自動化
-    watch: {
-        // オプションを使う場合はオブジェクト形式にする
-        todos: {
-            // 引数はウォッチしているプロパティの変更後の値
-            handler: function (todos) {
-                todoStorage.save(todos)
-            },
-            // deep オプションでネストしているデータも監視できる
-            deep: true
-        }
-    },
-
-    // step09_保存されたリストを取得しよう
-    created() {
-        // インスタンス作成時に自動的に fetch() する
-        this.todos = todoStorage.fetch()
-    },
-
-    // step07_リストへの追加
-    // step10_状態の変更と削除の処理
-    methods: {
-        doAdd: function (event, value) {
-            var comment = this.$refs.comment
-            if (!comment.value.length) {
-                return
-            }
-            // { 新しいID, コメント, 作業状態 }
-            // というオブジェクトを現在の todos リストへ push
-            // 作業状態「state」はデフォルト「作業中=0」で作成
-            this.todos.push({
-                id: todoStorage.uid++,
-                comment: comment.value,
-                state: 0
-            })
-            // フォーム要素を空にする
-            comment.value = ''
-        },
-
-        doChangeState: function (item) {
-            item.state = item.state ? 0 : 1
-        },
-
-        // 削除の処理
-        doRemove: function (item) {
-            var index = this.todos.indexOf(item)
-            this.todos.splice(index, 1)
-        }
-
-    }
-
-})
 
