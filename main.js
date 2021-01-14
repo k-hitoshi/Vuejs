4c9e16d1ff60622fa7a7ff5c87699d46f97227a0
kusimura <kusimura@esm.co.jp>
2021-01-14 11:56:17 +0900

    応用課題＿指摘対応②

--- main.js
+++ main.js
@@ -4,46 +4,41 @@ function doGet() {
 
 
 function inputSheet(array_ss) {
-  
   try {
-      const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
-      const lastrow = sheet.getLastRow();
-      const lastColumn = sheet.getLastColumn();
-      sheet.getRange(lastrow + 1, 1, 1, lastColumn).setValues(array_ss);
-      } catch(e) {
-          throw new Error('サーバエラーが発生しました');
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
+    const lastrow = sheet.getLastRow();
+    const lastColumn = sheet.getLastColumn();
+    sheet.getRange(lastrow + 1, 1, 1, lastColumn).setValues(array_ss);
+  } catch(e) {
+    throw new Error('サーバエラーが発生しました');
   }
-  
 }
 
 
 function deleteSheet(index) {
-  
   try {
-      const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
-      sheet.deleteRows(index + 2, 1);
-      } catch(e) {
-          throw new Error('サーバエラーが発生しました');
-  }
-
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
+    sheet.deleteRows(index + 2, 1);
+    } catch(e) {
+      throw new Error('サーバエラーが発生しました');
+    }
 }
 
 
 function changeState(index_Changestate) {
-  
   try {
-      const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
-      let range   = sheet.getRange(index_Changestate + 2, 3).getValue();
-      const lastColumn = sheet.getLastColumn();
-      if (range === 0) {
-          sheet.getRange(index_Changestate + 2, lastColumn).setValue(1)
-          } else {
-              sheet.getRange(index_Changestate + 2, lastColumn).setValue(0)
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('todoData');
+    const range = sheet.getRange(index_Changestate + 2, 3).getValue();
+    const lastColumn = sheet.getLastColumn();
+    const sheet_setRange = sheet.getRange(index_Changestate + 2, lastColumn)
+    if (range === 0) {
+      sheet_setRange.setValue(1)
+      } else {
+        sheet_setRange.setValue(0)
       }
-      } catch(e) {
-          throw new Error('サーバエラーが発生しました');    
+  } catch(e) {
+    throw new Error('サーバエラーが発生しました');    
   }
 
 }
-
 
